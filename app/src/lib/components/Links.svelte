ba47b65fbe0b3883d9570c2fbd71925d43d11e54
Ashley Cave <45659989+AHollowedHunter@users.noreply.github.com>
2022-12-20 23:54:47 +0000

    use svelte
    semi-copy existing design to change later

---
+++ app/src/lib/components/Links.svelte
@@ -0,0 +1,31 @@
+<ul>
+    <li id="🐈">
+        <a href="https://github.com/AHollowedHunter" target="_github"><img class="link-icon" src="images/github-white.ico" />&ensp;AHollowedHunter</a>
+    </li>
+    <li id="📷">
+        <a href="https://www.instagram.com/acave.photos/" target="_instagram"><img class="link-icon" src="images/instagram.png" />&ensp;@acave.photos</a>
+    </li>
+    <li id="💼">
+        <a href="https://www.linkedin.com/in/AshCave" target="_linkedin"><img class="link-icon" src="images/linkedin.png" />&ensp;AshCave</a>
+    </li>
+    <li id="🐘">
+        <a rel="me" target="_mastodon" href="https://universeodon.com/@AHollowedHunter"><img class="link-icon" src="images/mastodon.svg" />&ensp;AHollowedHunter</a>
+    </li>
+  </ul>
+
+<style>
+    ul {
+        list-style: none;
+        
+    }
+
+    a {
+         text-decoration: none;
+         color: ghostwhite;
+    }
+
+    .link-icon {
+  max-height: 1.1em;
+  vertical-align: middle;
+}
+</style>
---
+++ app/src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import "$lib/styles/normalize.css"
+    import "$lib/styles/skeleton.css"
+    import "$lib/styles/custom.css"
+	import Links from "$lib/components/Links.svelte";
+</script>
+
+<div style="justify-content: center;
+align-items: center; display: flex; height: 100vh;">
+    <div class="container">
+    <div class="row">
+        <div class="one-half column">
+            <h1>Ashley Cave</h1>
+            <h4>BSc Software Engineering Bournemouth University</h4>
+        </div>
+        <div class="one-half column">
+            <Links />
+        </div>
+    </div>
+</div>
+</div>
